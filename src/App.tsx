8a2baad4b87cbd914e082560b3e096983194c7cf
NguyenMinhHung <nmhung.7676@gmail.com>
2023-10-04 21:47:52 +0700

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {PersonList} from "./components/PersonList";
 import {Button} from "./components/Button";
 import {Input} from "./components/Input"
 import {Container} from "./components/Container.tsx";
+import {LoggedIn} from "./components/LoggedIn.tsx";
 function App() {
     const personName = {
         first: 'Minh',
@@ -35,6 +36,7 @@ function App() {
             }} />
             <Input value = '' handleChange={(event) => console.log(event)}/>
             <Container styles={{border: '1px solid black', padding:'1rem'}} />
+            <LoggedIn />
         </div>
     )
 }
