cc30b02781f473911607fcb266978a357ac3c75a
NguyenMinhHung <nmhung.7676@gmail.com>
2023-10-04 21:34:09 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import './main.scss'
+
+import { Greet} from "./components/Greet";
+import {Person} from "./components/Person";
+import {PersonList} from "./components/PersonList";
+import {Button} from "./components/Button";
+import {Input} from "./components/Input"
+import {Container} from "./components/Container.tsx";
+function App() {
+    const personName = {
+        first: 'Minh',
+        last: 'Hung'
+    }
+    const nameList = [
+        {
+            first: 'Thanh',
+            last: 'Thu'
+        },
+        {
+            first: 'Thu',
+            last: 'Hien'
+        },
+        {
+            first: 'Tien',
+            last: 'Dat'
+        }
+    ]
+    return (
+        <div className='App'>
+            <Greet name="Hung" messageCount={20} isLoggedIn={false}/>
+            <Person name={personName}/>
+            <PersonList names={nameList}/>
+            <Button handleClick={(event, id) => {
+                console.log("Hello Cac ban", event, id)
+            }} />
+            <Input value = '' handleChange={(event) => console.log(event)}/>
+            <Container styles={{border: '1px solid black', padding:'1rem'}} />
+        </div>
+    )
+}
+
+export default App
